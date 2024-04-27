589d255e06f2797ab65bbaee58366303b4b088c1
Tanya Misiura <tanjamiss1386@gmail.com>
2024-04-27 09:24:42 +0300

    fixing mentor's comments

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,7 @@
 function getElementWidth(content, padding, border) {
-  return parseFloat(content) + parseFloat(padding) + parseFloat(border);
+  return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
 }
 
-getElementWidth("50px", "8px", "4px"); 
-getElementWidth("60px", "12px", "8.5px"); 
-getElementWidth("200px", "0px", "0px"); 
-
-console.log(getElementWidth("50px", "8px", "4px")); // Повинно повертати 62
-console.log(getElementWidth("60px", "12px", "8.5px")); // Повинно повертати 80.5
-console.log(getElementWidth("200px", "0px", "0px")); // Повинно повертати 200
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
